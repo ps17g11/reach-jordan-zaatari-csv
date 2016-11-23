ac6a19663fef2130c812b56e473f6827708c12cf
Maxym Malynowsky <max@sparkgeo.com>
2016-11-23 17:08:08 +0200

    expand functions for clarity

--- src/syria-routes/index.js
+++ src/syria-routes/index.js
@@ -11,14 +11,23 @@ const [sheet1] = workbook.SheetNames;
 const originalCsv = XLSX.utils.sheet_to_csv(workbook.Sheets[sheet1]);
 const data = d3Dsv.csvParse(originalCsv);
 
-const csvData = data.map((row) => (
-  COLUMNS.ROUTES
+function filterNoData(row) {
+  return row.length > Object.keys(COLUMNS.HEADERS).length + 2;
+}
+
+function mapRows(row) {
+  return COLUMNS.ROUTES
     .map((route, index) => (
-      [index + 1, Object.values(route).map((attribute) => row[attribute] || '')].join(',')
+      [
+        index + 1,
+        ...Object.values(route).map((attribute) => row[attribute] || ''),
+      ].join(',')
     ))
-    .filter((subRow) => subRow.length > Object.keys(COLUMNS.HEADERS).length + 2)
-    .join('\n')
-)).join('\n');
+    .filter(filterNoData)
+    .join('\n');
+}
+
+const csvData = data.map(mapRows).join('\n');
 
 const headers = Object.values(COLUMNS.HEADERS).join(',');
 const newCsv = `Route,${headers}\n${csvData}`;
